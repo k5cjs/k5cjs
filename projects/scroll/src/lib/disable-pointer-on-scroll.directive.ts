90f5ad7cde38030e40f1f01099f1c6f2fb5f4ab6
kryshac <vladislav.tornea@gmail.com>
2022-03-08 19:14:26 +0200

    add scroll lib (#5)
    * add scroll lib
    
    * fix tests in scroll lib

---
+++ projects/scroll/src/lib/disable-pointer-on-scroll.directive.ts
@@ -0,0 +1,35 @@
+import { NumberInput, coerceNumberProperty } from '@angular/cdk/coercion';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+
+@Directive({
+  selector: '[k5cDisablePointerOnScroll]',
+})
+export class DisablePointerOnScrollDirective {
+  @Input()
+  set k5cDisablePointerOnScroll(value: NumberInput) {
+    this._disablePointerOnScroll = coerceNumberProperty(value, 150);
+  }
+  get k5cDisablePointerOnScroll(): number {
+    return this._disablePointerOnScroll;
+  }
+  private _disablePointerOnScroll!: number;
+
+  @Output() scrollPosition: EventEmitter<number> = new EventEmitter<number>();
+
+  private _timer!: ReturnType<typeof setTimeout>;
+
+  constructor(private _el: ElementRef<HTMLElement>) {}
+
+  @HostListener('scroll', ['$event']) onclick = () => {
+    this.scrollPosition.emit(this._el.nativeElement.scrollTop);
+
+    clearTimeout(this._timer);
+
+    this._el.nativeElement.style.pointerEvents = 'none';
+
+    this._timer = setTimeout(
+      () => (this._el.nativeElement.style.pointerEvents = 'initial'),
+      this.k5cDisablePointerOnScroll,
+    );
+  };
+}
---
+++ projects/scroll/src/lib/scroll.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { DisablePointerOnScrollDirective } from './disable-pointer-on-scroll.directive';
+
+@NgModule({
+  declarations: [DisablePointerOnScrollDirective],
+  exports: [DisablePointerOnScrollDirective],
+})
+export class K5cScrollModule {}
