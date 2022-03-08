a68cefd679c2522a53d7032f668b20c51dca2b53
Cristian Cazacu <vladislav.tornea@gmail.com>
2022-03-08 17:34:08 +0200

    add scroll lib

---
+++ projects/scroll/src/lib/disable-pointer-on-scroll.directive.spec.ts
@@ -0,0 +1,136 @@
+import { ChangeDetectionStrategy, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { K5cScrollModule } from './scroll.module';
+
+describe('SelectionModel', () => {
+  let component: DummyComponent;
+  let fixture: ComponentFixture<DummyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DummyComponent, Dummy2Component],
+      imports: [K5cScrollModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DummyComponent);
+    fixture.detectChanges();
+
+    component = fixture.componentInstance;
+  });
+
+  it('default time', (done) => {
+    component.element(1).scrollIntoView({ behavior: 'smooth' });
+    setTimeout(() => expect(component.pointerEvents()).toEqual('none'), 50);
+    setTimeout(() => expect(component.pointerEvents()).toEqual('initial'), 600);
+    setTimeout(() => done(), 610);
+  });
+
+  it('custom time', (done) => {
+    const fixture = TestBed.createComponent(Dummy2Component);
+
+    fixture.detectChanges();
+
+    const component = fixture.componentInstance;
+
+    component.element(1).scrollIntoView({ behavior: 'smooth' });
+    setTimeout(() => expect(component.pointerEvents()).toEqual('none'), 50);
+    setTimeout(() => expect(component.pointerEvents()).toEqual('none'), 1800);
+    setTimeout(() => expect(component.pointerEvents()).toEqual('initial'), 1900);
+    setTimeout(() => done(), 1910);
+  });
+});
+
+@Component({
+  selector: 'k5c-dummy',
+  template: `
+    <div #container class="container" k5cDisablePointerOnScroll>
+      <div #item *ngFor="let i of list" class="container__item"></div>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        display: flex;
+        flex-direction: column;
+        background: green;
+        width: 300px;
+        height: 300px;
+        overflow: auto;
+      }
+
+      .container__item {
+        flex-shrink: 0;
+        height: 30px;
+        background: red;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+class DummyComponent {
+  @ViewChild('container', { static: true }) container!: ElementRef<HTMLElement>;
+  @ViewChildren('item') items!: QueryList<ElementRef<HTMLElement>>;
+
+  list: string[];
+
+  constructor() {
+    this.list = Array.from({ length: 20 }).map(() => '');
+  }
+
+  pointerEvents(): string {
+    return this.container.nativeElement.style.getPropertyValue('pointer-events');
+  }
+
+  element(index: number): HTMLElement {
+    return this.items.get(index)!.nativeElement;
+  }
+}
+
+@Component({
+  selector: 'k5c-dummy',
+  template: `
+    <div #container class="container" [k5cDisablePointerOnScroll]="1500">
+      <div #item *ngFor="let i of list" class="container__item"></div>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        display: flex;
+        flex-direction: column;
+        background: green;
+        width: 300px;
+        height: 300px;
+        overflow: auto;
+      }
+
+      .container__item {
+        flex-shrink: 0;
+        height: 30px;
+        background: red;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+class Dummy2Component {
+  @ViewChild('container', { static: true }) container!: ElementRef<HTMLElement>;
+  @ViewChildren('item') items!: QueryList<ElementRef<HTMLElement>>;
+
+  list: string[];
+
+  constructor() {
+    this.list = Array.from({ length: 20 }).map(() => '');
+  }
+
+  pointerEvents(): string {
+    return this.container.nativeElement.style.getPropertyValue('pointer-events');
+  }
+
+  element(index: number): HTMLElement {
+    return this.items.get(index)!.nativeElement;
+  }
+}
---
+++ projects/scroll/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of scroll
+ */
+export * from './lib/disable-pointer-on-scroll.directive';
+export * from './lib/scroll.module';
