a475e95ac6a55dd2a43965d9ef22d47c9994364c
Cristian Cazacu <vladislav.tornea@gmail.com>
2023-02-12 15:56:04 +0200

    implement focus and blur on select

---
+++ projects/select/src/lib/config/positions.config.ts
@@ -0,0 +1,28 @@
+import { ConnectedPosition } from '@angular/cdk/overlay';
+
+export const DEFAULT_POSITIONS: ConnectedPosition[] = [
+  {
+    originX: 'start',
+    originY: 'bottom',
+    overlayX: 'start',
+    overlayY: 'top',
+  },
+  {
+    originX: 'start',
+    originY: 'top',
+    overlayX: 'start',
+    overlayY: 'bottom',
+  },
+  {
+    originX: 'end',
+    originY: 'top',
+    overlayX: 'end',
+    overlayY: 'bottom',
+  },
+  {
+    originX: 'end',
+    originY: 'bottom',
+    overlayX: 'end',
+    overlayY: 'top',
+  },
+];
