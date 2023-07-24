99724f90bc56269f6dde70f0c71556cfab4d3574
Cristian Cazacu <vladislav.tornea@gmail.com>
2023-07-24 23:17:44 +0300

    fix tests for examples

--- projects/examples/src/app/views/cal/cal.component.spec.ts
+++ projects/examples/src/app/views/cal/cal.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { KcCalModule } from '@k5cjs/cal';
+
 import { CalComponent } from './cal.component';
 
 describe('CalComponent', () => {
@@ -9,6 +11,7 @@ describe('CalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CalComponent],
+      imports: [KcCalModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CalComponent);
