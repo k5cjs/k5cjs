715122f4b99085170c26bed30441cef0cf7bfc80
Cristian Cazacu <vladislav.tornea@gmail.com>
2023-08-02 23:25:04 +0300

    add test for animations

--- projects/animations/src/lib/state-change-fn.ts
+++ projects/animations/src/lib/state-change-fn.ts
@@ -1,43 +1,66 @@
+const enum DefaultState {
+  Void = 'void',
+  Null = 'null',
+}
+
+const enum BooleanState {
+  False = 'false',
+  True = 'true',
+}
+
+const hideStates = [DefaultState.Void, BooleanState.False];
+const showStates = [DefaultState.Null, BooleanState.True];
+const allStates = [...hideStates, ...showStates];
+
 export const stateChangeEnter = (fromState: string, toState: string) => {
-  const fromStateString = String(fromState);
-  const toStateString = String(toState);
-
-  if (fromStateString === toStateString) return false;
-  /**
-   * this is use when user use animation [@modal]="true | false"
-   */
-  if (fromStateString === 'void' && toStateString === 'false') return false;
-
-  return (
-    /**
-     * this is use when user use animation @modal
-     */
-    (fromStateString === 'void' && toStateString === 'null') ||
-    /**
-     * this is use when user use animation [@modal]="true | false"
-     */
-    (fromStateString === 'false' && toStateString === 'true')
-  );
-};
-
-export const stateChangeLeave = (fromState: string, toState: string) => {
-  const fromStateString = String(fromState);
-  const toStateString = String(toState);
+  const fromStateString = String(fromState) as DefaultState | BooleanState;
+  const toStateString = String(toState) as DefaultState | BooleanState;
 
   if (fromStateString === toStateString) return false;
   /**
    * this is use when user use boolean value
+   * and first state is void
+   * and second state is false
+   * so we don't need to animate
    */
-  if (fromStateString === 'void' && toStateString === 'false') return false;
+  if (fromStateString === DefaultState.Void && toStateString === BooleanState.False) return false;
 
   return (
+    (hideStates.includes(fromStateString) && showStates.includes(toStateString)) ||
     /**
-     * this is use when user use animation @modal
+     * this is use when user use animation [@modal]="{ value: unknown }"
      */
-    (fromStateString === 'null' && toStateString === 'void') ||
-    /**
-     * this is use when user use animation [@modal]="true | false"
-     */
-    (fromStateString === 'true' && toStateString === 'false')
+    (hideStates.includes(fromStateString) && !allStates.includes(toStateString))
   );
+};
+
+export const stateChangeLeave = (fromState: string, toState: string) => {
+  const fromStateString = String(fromState) as DefaultState | BooleanState;
+  const toStateString = String(toState) as DefaultState | BooleanState;
+
+  if (fromStateString === toStateString) return false;
+  /**
+   * this is use when user use boolean value
+   * and first state is void
+   * and second state is false
+   * so we don't need to animate
+   */
+  if (fromStateString === DefaultState.Void && toStateString === BooleanState.False) return false;
+
+  return (
+    (showStates.includes(fromStateString) && hideStates.includes(toStateString)) ||
+    /**
+     * this is use when user use animation [@modal]="{ value: unknown }"
+     */
+    (!allStates.includes(fromStateString) && hideStates.includes(toStateString))
+  );
+};
+
+export const stateChanged = (fromState: string, toState: string) => {
+  const fromStateString = String(fromState) as DefaultState | BooleanState;
+  const toStateString = String(toState) as DefaultState | BooleanState;
+
+  if (allStates.includes(fromStateString) || allStates.includes(toStateString)) return false;
+
+  return fromStateString !== toStateString;
 };
---
+++ projects/animations/src/lib/toggle-y.spec.ts
@@ -0,0 +1,212 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { toggleY } from './toggle-y';
+
+@Component({
+  template: `
+    <div id="state1" *ngIf="state1" @toggleY>Default state</div>
+    <div id="state2" *ngIf="state2" [@toggleY]="state2">Boolean state</div>
+    <div id="state3" #ref *ngIf="state3" [@toggleY]="{ value: message, params: { height: ref.offsetHeight } }">
+      {{ message }}
+    </div>
+  `,
+  styles: [
+    `
+      div {
+        width: 200px;
+        font-size: 16px;
+        line-height: 16px;
+      }
+    `,
+  ],
+  animations: [toggleY(40)],
+})
+class DumpyComponent {
+  state1 = false;
+  state2 = false;
+  state3 = false;
+
+  message = 'Hello world';
+}
+
+@Component({
+  template: `
+    <div id="state1" *ngIf="state1" @toggleY>Default state</div>
+  `,
+  styles: [
+    `
+      div {
+        width: 200px;
+        font-size: 16px;
+        line-height: 16px;
+      }
+    `,
+  ],
+  animations: [toggleY()],
+})
+class Dumpy1Component {
+  state1 = false;
+}
+
+describe('Toggle y', () => {
+  let component: DumpyComponent;
+  let fixture: ComponentFixture<DumpyComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DumpyComponent],
+      imports: [BrowserAnimationsModule],
+      teardown: {
+        destroyAfterEach: true,
+      },
+    });
+    fixture = TestBed.createComponent(DumpyComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create an instance', () => {
+    void expect(component).toBeTruthy();
+  });
+
+  it('default show', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    component.state1 = true;
+
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state1')!.offsetHeight).toEqual(0);
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state1')!.offsetHeight).toBeGreaterThan(0);
+  });
+
+  it('default hide', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    component.state1 = true;
+    fixture.detectChanges();
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state1')!.offsetHeight).toBeGreaterThan(0);
+
+    component.state1 = false;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state1')!.offsetHeight).toBeGreaterThanOrEqual(8);
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state1')).toBeNull();
+  });
+
+  it('boolean show', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    component.state2 = true;
+
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state2')!.offsetHeight).toEqual(0);
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state2')!.offsetHeight).toBeGreaterThan(0);
+  });
+
+  it('boolean hide', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    component.state2 = true;
+    fixture.detectChanges();
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state2')!.offsetHeight).toBeGreaterThan(0);
+
+    component.state2 = false;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state2')!.offsetHeight).toBeGreaterThanOrEqual(8);
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state2')).toBeNull();
+  });
+
+  it('increase size', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    component.state3 = true;
+    component.message = 'Hello world 2';
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state3')!.offsetHeight).toEqual(0);
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state3')!.offsetHeight).toBeGreaterThan(0);
+
+    component.message = 'Hello world big message two line';
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state3')!.offsetHeight).toBeLessThanOrEqual(16);
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state3')!.offsetHeight).toBeGreaterThan(16);
+  });
+
+  it('unknown hide', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    component.state3 = true;
+    component.message = 'Hello world 2';
+    fixture.detectChanges();
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    component.message = 'Hello world big message two line';
+    fixture.detectChanges();
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state3')!.offsetHeight).toBeGreaterThan(16);
+
+    component.state3 = false;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state3')!.offsetHeight).toBeGreaterThanOrEqual(24);
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    expect(compiled.querySelector<HTMLElement>('#state1')).toBeNull();
+  });
+
+  it('unknown hide', async () => {
+    TestBed.resetTestingModule();
+
+    TestBed.configureTestingModule({
+      declarations: [Dumpy1Component],
+      imports: [BrowserAnimationsModule],
+      teardown: {
+        destroyAfterEach: true,
+      },
+    });
+    const fixture = TestBed.createComponent(Dumpy1Component);
+    const component = fixture.componentInstance;
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    component.state1 = true;
+
+    fixture.detectChanges();
+
+    expect(compiled.querySelector<HTMLElement>('#state1')!.offsetHeight).toEqual(0);
+
+    await new Promise((resolve) => setTimeout(resolve, 50));
+
+    expect(compiled.querySelector<HTMLElement>('#state1')!.offsetHeight).toBeLessThanOrEqual(8);
+
+    await new Promise((resolve) => setTimeout(resolve, 50));
+
+    expect(compiled.querySelector<HTMLElement>('#state1')!.offsetHeight).toBeGreaterThanOrEqual(12);
+  });
+});
