d8f8625dd9fcbfcf4899c29056d0e81a96a713d6
Cristian Cazacu <vladislav.tornea@gmail.com>
2023-08-11 01:03:51 +0300

    test coverage for monorepo

--- projects/form-field/karma.conf.js
+++ projects/form-field/karma.conf.js
@@ -1,55 +1,13 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+const defaultConfig = require('../../karma.conf.js');
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-coverage-istanbul-reporter'),
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/form-field'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'json-summary' }],
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Firefox', 'Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
-      },
-    },
-    singleRun: false,
-    restartOnFileChange: true,
-    formatError: (msg) => msg.replace(/http:\/\/localhost:9876\/_karma_webpack_\//g, ''),
+  const baseConfig = defaultConfig(config);
 
-    ...(process.env.CI && {
-      singleRun: true,
-      autoWatch: false,
-    }),
+  config.set({
+    ...baseConfig,
+    coverageReporter: {
+      ...baseConfig.coverageReporter,
+      dir: require('path').join(__dirname, '../../coverage/form-field'),
+    },
   });
 };
