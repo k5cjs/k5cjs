df5a0d2936f83f58236925bdc81776fa3aa26cc7
Cristian Cazacu <vladislav.tornea@gmail.com>
2023-08-21 17:56:14 +0300

    add animation for slide right to left

--- projects/examples/src/app/views/select/views/form-field/form-field.component.spec.ts
+++ projects/examples/src/app/views/select/views/form-field/form-field.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { KcFormField } from '@k5cjs/form-field';
+import { KcSelectModule } from '@k5cjs/select';
 
 import { FormFieldComponent } from './form-field.component';
 
@@ -9,6 +13,7 @@ describe('FormFieldComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FormFieldComponent],
+      imports: [KcFormField, KcSelectModule, ReactiveFormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FormFieldComponent);
