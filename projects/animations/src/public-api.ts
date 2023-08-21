db92e918fb30e88c1786ed38ff0a0be4c1be4432
Cristian Cazacu <vladislav.tornea@gmail.com>
2023-08-21 18:20:51 +0300

    export slide animation

--- projects/animations/src/public-api.ts
+++ projects/animations/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/block-initial-render';
 export * from './lib/ng-if';
+export * from './lib/slide-r-to-l';
 export * from './lib/stagger-child';
 export * from './lib/state-change-fn';
 export * from './lib/toggle-o';
