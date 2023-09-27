42c9a70ed573497b49fa5bab8ce7938ea6b8584a
niksonxs <66615963+niksonxs@users.noreply.github.com>
2023-09-28 00:53:33 +0300

    fix selector intersection (#42)

--- projects/cal/src/lib/services/kc-cal-selector/kc-cal-selector.service.spec.ts
+++ projects/cal/src/lib/services/kc-cal-selector/kc-cal-selector.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Selector } from '../../types';
 import { KcCal } from '../kc-cal/kc-cal.service';
 
 import { KcCalSelector } from './kc-cal-selector.service';
@@ -16,5 +17,23 @@ describe('KcCalSelector', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should toggle selector after select (normal flow)', () => {
+    service.select(new Date('08/10/2023'));
+    service.select(new Date('09/10/2023'));
+    expect(service['_selector']).toEqual(Selector.From);
+    service.select(new Date('09/05/2023'));
+    //from < to (should toggle)
+    expect(service['_selector']).toEqual(Selector.To);
+  });
+
+  it('should keep selector after selecting intersected range', () => {
+    service.select(new Date('08/10/2023'));
+    service.select(new Date('09/10/2023'));
+    expect(service['_selector']).toEqual(Selector.From);
+    service.select(new Date('09/15/2023'));
+    //from > to (should keep)
+    expect(service['_selector']).toEqual(Selector.From);
   });
 });
