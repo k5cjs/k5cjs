8f1422e3696849362ac2f02dd7bbc11737345c32
vladPopovschii <64737767+vladPopovschii@users.noreply.github.com>
2024-02-09 11:46:39 +0200

    Added drodown on close (#75)

--- projects/dropdown/src/lib/components/dropdown/dropdown.component.ts
+++ projects/dropdown/src/lib/components/dropdown/dropdown.component.ts
@@ -1,5 +1,5 @@
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { ChangeDetectionStrategy, Component, Input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 import { KcInternalDropdownComponent } from '../internal-dropdown/internal-dropdown.component';
 
@@ -18,6 +18,12 @@ export class KcDropdownComponent {
   // prettier-ignore
   set preventClose(value: boolean | string) { this._preventClose = coerceBooleanProperty(value); }
   private _preventClose = true;
+
+  @Output() dialogClosed = new EventEmitter<void>();
+
+  emitClose(): void {
+    this.dialogClosed.emit();
+  }
 
   open(): void {
     this.internalDropdown.open();
